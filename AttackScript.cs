a07c0d9f1a75fdea8c0e3837458d9c9ade85346f
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-03 23:48:03 +0300

    Update AttackScript.cs

--- AttackScript.cs
+++ AttackScript.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    public Transform firePoint;
+    
+
     private Animator animator;
     private PlayerMovement playerMovement;
-    private float cooldownTimer;
+
+    public GameObject fireball;
+
+    private float cooldownTimer1;
+    private float cooldownTimer2;
 
     private void Awake()
     {
@@ -18,18 +25,36 @@ public class AttackScript : MonoBehaviour
     private void Update()
     {
         // Increase cooldownTimer by Time.deltaTime
-        cooldownTimer += Time.deltaTime;
+        cooldownTimer1 += Time.deltaTime;
+        cooldownTimer2 += Time.deltaTime;
 
         // Check if the attack button is pressed, player is grounded, and cooldownTimer has reached attackCooldown
-        if (Input.GetMouseButton(0) && playerMovement.grounded && cooldownTimer >= attackCooldown)
+        if (Input.GetMouseButton(0) && playerMovement.grounded && cooldownTimer1 >= attackCooldown)
         {
             Attack();
+        }
+
+        if (Input.GetMouseButton(1) && playerMovement.grounded && cooldownTimer2 >= attackCooldown)
+        {
+            Fire();
         }
     }
 
     private void Attack()
     {
         animator.SetTrigger("attack");
-        cooldownTimer = 0; // Reset cooldownTimer to zero after the attack
+        cooldownTimer1 = 0; //Reset cooldown timer to zero after the attack
     }
+
+    private void Fire()
+    {
+        animator.SetTrigger("fire_projectile");
+        cooldownTimer2 = 0;
+
+        Instantiate(fireball, firePoint.position, firePoint.rotation);
+        
+    }
+    
+
 }
+
