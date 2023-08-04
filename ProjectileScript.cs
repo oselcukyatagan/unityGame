07361bb70fe579b7321d947d4fecf1ce58973d18
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-04 11:07:49 +0300

    Update ProjectileScript.cs

--- ProjectileScript.cs
+++ ProjectileScript.cs
@@ -1,7 +1,4 @@
 using UnityEngine;
-using UnityEngine.Playables;
-using System.Collections;
-
 
 public class ProjectileScript : MonoBehaviour
 {
@@ -9,25 +6,33 @@ public class ProjectileScript : MonoBehaviour
     public float speed = 10f;
     [SerializeField] public float time = 0.8f;
 
-    private BoxCollider2D boxCollider;
     private Animator animator;
 
     public bool hit = false;
 
+    public Transform playerTransform;
+    public float direction;
+
 
     private void Awake()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
+    {  
         animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (hit != true)
-        {
-            Vector2 bulletMovement = new Vector2(speed * Time.deltaTime, 0f);
-            transform.Translate(bulletMovement);
-        }
+        if (hit) return;
+
+        // Get Player Location
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        direction = Mathf.Sign(playerTransform.localScale.x);
+
+        Vector2 bulletMovement = new Vector2(speed * Time.deltaTime * direction, 0f);
+        transform.Translate(bulletMovement);
+
+        transform.localScale = new Vector3(direction * 0.8f, 0.8f, 1);
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,5 +41,6 @@ public class ProjectileScript : MonoBehaviour
         animator.SetTrigger("fireballExplode");
         Destroy(gameObject, time);
     }
+
 
 }
