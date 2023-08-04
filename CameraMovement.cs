11a6f84a5ad67d8261e67856115c9fd335c55be8
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-04 11:30:48 +0300

    Update CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,60 +1,17 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class ProjectileScript : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed = 10f;
-    [SerializeField] public float time = 0.8f;
 
-    private Animator animator;
-
-    public bool hit = false;
-
-    public Transform playerTransform;
-    public float direction;
-    public float projectileTimer;
-
-    private void Awake()
-    {  
-        animator = GetComponent<Animator>();
-
-        // Get Player Location
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        direction = Mathf.Sign(playerTransform.localScale.x);
-
-        // Direction of the projectile
-        transform.localScale = new Vector3(direction * 0.8f, 0.8f, 1);
-
-    }
+    public Transform player;
+    
 
     private void Update()
     {
-        if (hit) return;
-
-        // Move projectile
-        Vector2 bulletMovement = new Vector2(speed * Time.deltaTime * direction, 0f);
-        transform.Translate(bulletMovement);
-
-        // After 8 seconds, destroy projectile.
-        projectileTimer += Time.deltaTime;
-
-        if (projectileTimer > 8f)
-            handleProjectile();
-
-
+        
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 
-    // When object hits something, this function is called.
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        hit = true;
-        animator.SetTrigger("fireballExplode");
-        Destroy(gameObject, time);
-    }
-
-    private void handleProjectile()
-    {
-        Destroy(gameObject);
-    }
 
 }
