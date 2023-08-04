01b978da0e9e15f96a35ef005f4be06f9cfa9167
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-04 11:16:17 +0300

    did it all by myself, proud of it fr

--- ProjectileScript.cs
+++ ProjectileScript.cs
@@ -12,7 +12,7 @@ public class ProjectileScript : MonoBehaviour
 
     public Transform playerTransform;
     public float direction;
-
+    public float projectileTimer;
 
     private void Awake()
     {  
@@ -27,14 +27,25 @@ public class ProjectileScript : MonoBehaviour
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         direction = Mathf.Sign(playerTransform.localScale.x);
 
+        // Move projectile
         Vector2 bulletMovement = new Vector2(speed * Time.deltaTime * direction, 0f);
         transform.Translate(bulletMovement);
 
+        // Direction of the projectile
         transform.localScale = new Vector3(direction * 0.8f, 0.8f, 1);
+
+
+
+        // After 8 seconds, destroy projectile.
+        projectileTimer += Time.deltaTime;
+
+        if (projectileTimer > 8f)
+            handleProjectile();
 
 
     }
 
+    // When object hits something, this function is called.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         hit = true;
@@ -42,5 +53,9 @@ public class ProjectileScript : MonoBehaviour
         Destroy(gameObject, time);
     }
 
+    private void handleProjectile()
+    {
+        Destroy(gameObject);
+    }
 
 }
