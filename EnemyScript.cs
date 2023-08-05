e1681f2c4b74169acd2beaba696002949d9df3ce
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-05 13:57:52 +0300

    Create EnemyScript.cs

---
+++ EnemyScript.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float enemyHealth = 100;
+    public Animator enemyAnimator;
+    public Rigidbody2D enemyBody;
+
+    private void Awake()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        enemyBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void enemyTakeDamage(float damage)
+    {
+        enemyHealth -= damage;
+
+        if (enemyHealth <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
