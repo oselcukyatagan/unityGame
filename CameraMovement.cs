55845dc7ab43c4795e649932ebca0daa478f00aa
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-08-06 17:50:25 +0300

    I'm a genious

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,16 +1,26 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
 
     public Transform player;
+    public Transform enemy;
     
+
 
     private void Update()
     {
+        if (player != null && enemy != null)
+            transform.position = new Vector3((player.position.x + enemy.position.x) / 2, transform.position.y, transform.position.z);
         
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        if (player == null && enemy != null)
+            transform.position = new Vector3(enemy.position.x, transform.position.y, transform.position.z);
+
+        if (player != null && enemy == null)
+            transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+
+        if (player == null && enemy == null)
+            transform.position = new Vector3(0, 0, 0);
     }
 
 
